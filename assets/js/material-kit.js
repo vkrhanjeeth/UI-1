40b64171ea720a2110de73b9d52d6ec4a9148580
vkrhanjeeth <vkrhanjeeth94@gmail.com>
2018-11-10 13:57:06 +0530

    Add Files

---
+++ assets/js/material-kit.js
@@ -0,0 +1,220 @@
+
+var big_image;
+
+$(document).ready(function () {
+
+  // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
+  $('body').bootstrapMaterialDesign();
+
+  window_width = $(window).width();
+
+  $navbar = $('.navbar[color-on-scroll]');
+  scroll_distance = $navbar.attr('color-on-scroll') || 500;
+
+  $navbar_collapse = $('.navbar').find('.navbar-collapse');
+
+  //  Activate the Tooltips
+  $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
+
+  // Activate Popovers
+  $('[data-toggle="popover"]').popover();
+
+  if ($('.navbar-color-on-scroll').length != 0) {
+    $(window).on('scroll', materialKit.checkScrollForTransparentNavbar);
+  }
+
+  materialKit.checkScrollForTransparentNavbar();
+
+  if (window_width >= 768) {
+    big_image = $('.page-header[data-parallax="true"]');
+    if (big_image.length != 0) {
+      $(window).on('scroll', materialKit.checkScrollForParallax);
+    }
+
+  }
+
+
+});
+    
+$(document).ready(function () {
+  $('#example').DataTable();
+});
+
+
+$(document).on('click', '.navbar-toggler', function () {
+  $toggle = $(this);
+
+  if (materialKit.misc.navbar_menu_visible == 1) {
+    $('html').removeClass('nav-open');
+    materialKit.misc.navbar_menu_visible = 0;
+    $('#bodyClick').remove();
+    setTimeout(function () {
+      $toggle.removeClass('toggled');
+    }, 550);
+
+    $('html').removeClass('nav-open-absolute');
+  } else {
+    setTimeout(function () {
+      $toggle.addClass('toggled');
+    }, 580);
+
+
+    div = '<div id="bodyClick"></div>';
+    $(div).appendTo("body").click(function () {
+      $('html').removeClass('nav-open');
+
+      if ($('nav').hasClass('navbar-absolute')) {
+        $('html').removeClass('nav-open-absolute');
+      }
+      materialKit.misc.navbar_menu_visible = 0;
+      $('#bodyClick').remove();
+      setTimeout(function () {
+        $toggle.removeClass('toggled');
+      }, 550);
+    });
+
+    if ($('nav').hasClass('navbar-absolute')) {
+      $('html').addClass('nav-open-absolute');
+    }
+
+    $('html').addClass('nav-open');
+    materialKit.misc.navbar_menu_visible = 1;
+  }
+});
+
+materialKit = {
+  misc: {
+    navbar_menu_visible: 0,
+    window_width: 0,
+    transparent: true,
+    fixedTop: false,
+    navbar_initialized: false,
+    isWindow: document.documentMode || /Edge/.test(navigator.userAgent)
+  },
+
+  initFormExtendedDatetimepickers: function () {
+    $('.datetimepicker').datetimepicker({
+      icons: {
+        time: "fa fa-clock-o",
+        date: "fa fa-calendar",
+        up: "fa fa-chevron-up",
+        down: "fa fa-chevron-down",
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+      }
+    });
+    $('.datepicker').datetimepicker({
+      format: 'DD/MM/YYYY',
+      icons: {
+        time: "fa fa-clock-o",
+        date: "fa fa-calendar",
+        up: "fa fa-chevron-up",
+        down: "fa fa-chevron-down",
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+      }
+    });
+  },
+
+  initSliders: function () {
+    // Sliders for demo purpose
+    var slider = document.getElementById('sliderRegular');
+
+    noUiSlider.create(slider, {
+      start: 40,
+      connect: [true, false],
+      range: {
+        min: 0,
+        max: 100
+      }
+    });
+
+    var slider2 = document.getElementById('sliderDouble');
+
+    noUiSlider.create(slider2, {
+      start: [20, 60],
+      connect: true,
+      range: {
+        min: 0,
+        max: 100
+      }
+    });
+  },
+
+  checkScrollForParallax: function () {
+    oVal = ($(window).scrollTop() / 3);
+    big_image.css({
+      'transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-o-transform': 'translate3d(0,' + oVal + 'px,0)'
+    });
+  },
+
+  checkScrollForTransparentNavbar: debounce(function () {
+    if ($(document).scrollTop() > scroll_distance) {
+      if (materialKit.misc.transparent) {
+        materialKit.misc.transparent = false;
+        $('.navbar-color-on-scroll').removeClass('navbar-transparent');
+      }
+    } else {
+      if (!materialKit.misc.transparent) {
+        materialKit.misc.transparent = true;
+        $('.navbar-color-on-scroll').addClass('navbar-transparent');
+      }
+    }
+  }, 17)
+};
+
+
+md = {
+  misc: {
+    navbar_menu_visible: 0,
+    active_collapse: true,
+    disabled_collapse_init: 0
+  },
+
+  showNotification: function (from, align) {
+    type = ['', 'info', 'danger', 'success', 'warning', 'primary'];
+
+    color = Math.floor((Math.random() * 5) + 1);
+
+    $.notify({
+      icon: "add_alert",
+      message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+
+    }, {
+      type: type[color],
+      timer: 3000,
+      placement: {
+        from: from,
+        align: align
+      }
+    });
+  },
+
+}
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+};
